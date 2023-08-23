ffe8097f3778cf5b708be4bf1d69063f4b9683d5
hotza1235 <hotgabpom@gmail.com>
2023-08-23 13:31:21 +0700

    modifield reg

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -1,54 +1,52 @@
 package entity
 
 import (
+	"time"
 
-           "gorm.io/gorm"
-           "time"
-
-           "golang.org/x/crypto/bcrypt"
-           "gorm.io/driver/sqlite"
-
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
- 
+
 var db *gorm.DB
 
 func DB() *gorm.DB {
 
-           return db
+	return db
 
 }
 
 func SetupDatabase() {
 
-  database, err := gorm.Open(sqlite.Open("TMS.db"), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("TMS.db"), &gorm.Config{})
 
-  if err != nil {
+	if err != nil {
 
-        panic("failed to connect database")
+		panic("failed to connect database")
 
-  }
+	}
 
-  // Migrate the schema
+	// Migrate the schema
 
-  database.AutoMigrate(
-    // Actor
-    &Admin{},
-    &Member{},
-    &Employee{},
-    // Main Entity
-    &Gender{},
-    &Course{},
-    &CourseRegistration{},
-    &PaymentStatus{},
-    &PaymentCheck {},
-    &Question{},
-    &Reply{},
-    &News{},
-  )
+	database.AutoMigrate(
+		// Actor
+		&Admin{},
+		&Member{},
+		&Employee{},
+		// Main Entity
+		&Gender{},
+		&Course{},
+		&CourseRegistration{},
+		&PaymentStatus{},
+		&PaymentCheck{},
+		&Question{},
+		&Reply{},
+		&News{},
+	)
 
-  db = database
+	db = database
 
-  passwordA, _ := bcrypt.GenerateFromPassword([]byte("123456"), 14)
+	passwordA, _ := bcrypt.GenerateFromPassword([]byte("123456"), 14)
 
 	AdminA := Admin{
 		Email:    "Admin01@example.com",
@@ -57,90 +55,90 @@ func SetupDatabase() {
 	}
 	db.Model(&Admin{}).Create(&AdminA)
 
-  GenderA := Gender{
-		Name:     "ชาย",
+	GenderA := Gender{
+		Name: "ชาย",
 	}
 	db.Model(&Gender{}).Create(&GenderA)
 
-  GenderB := Gender{
-		Name:     "หญิง",
+	GenderB := Gender{
+		Name: "หญิง",
 	}
 	db.Model(&Gender{}).Create(&GenderB)
 
-  GenderC := Gender{
-		Name:     "ไม่ระบุ",
+	GenderC := Gender{
+		Name: "ไม่ระบุ",
 	}
 	db.Model(&Gender{}).Create(&GenderC)
 
-  BirthdayA := time.Date(2017, time.November, 4, 9, 15, 0, 0, time.UTC)
+	BirthdayA := time.Date(2017, time.November, 4, 9, 15, 0, 0, time.UTC)
 
-  MemberA := Member{
-    Email:    "Member01@example.com",
-    Name:     "สมาชิก01",
-    Password: string(passwordA),
-    Image:    "https://img.freepik.com/free-icon/group-profile-users_318-41953.jpg?w=2000",
-    Birthday: BirthdayA,
-    Gender:   GenderA,
-  }
-  db.Model(&Member{}).Create(&MemberA)
+	MemberA := Member{
+		Email:    "Member01@example.com",
+		Name:     "สมาชิก01",
+		Password: string(passwordA),
+		Image:    "https://img.freepik.com/free-icon/group-profile-users_318-41953.jpg?w=2000",
+		Birthday: BirthdayA,
+		Gender:   GenderA,
+	}
+	db.Model(&Member{}).Create(&MemberA)
 
-  EmployeeA := Employee{
-    Email:    "Employee01@example.com",
-    Name:     "พนักงาน01",
-    Password: string(passwordA),
-    Image:    "https://cdn-icons-png.flaticon.com/512/3789/3789820.png",
-  }
-  db.Model(&Employee{}).Create(&EmployeeA)
+	EmployeeA := Employee{
+		Email:    "Employee01@example.com",
+		Name:     "พนักงาน01",
+		Password: string(passwordA),
+		Image:    "https://cdn-icons-png.flaticon.com/512/3789/3789820.png",
+	}
+	db.Model(&Employee{}).Create(&EmployeeA)
 
-  CourseA := Course{
-    Name:     "คอร์สทดสอบ",
-    Detail:   "test test test",
-    Price:    1000,
-    Image:    "https://tipa.in/wp-content/uploads/2021/05/Online-courses.jpg",
-    Employee: EmployeeA,
-  }
-  db.Model(&Course{}).Create(&CourseA)
+	CourseA := Course{
+		Name:     "คอร์สทดสอบ",
+		Detail:   "test test test",
+		Price:    1000,
+		Image:    "https://tipa.in/wp-content/uploads/2021/05/Online-courses.jpg",
+		Employee: EmployeeA,
+	}
+	db.Model(&Course{}).Create(&CourseA)
 
-  CourseRegistrationA := CourseRegistration{
-    Receipt:  "https://img.freepik.com/free-vector/realistic-receipt-template_23-2147938550.jpg?w=2000",
-    Member:   MemberA,
-    Course:   CourseA,
-  }
-  db.Model(&CourseRegistration{}).Create(&CourseRegistrationA)
+	CourseRegistrationA := CourseRegistration{
+		Receipt: "https://img.freepik.com/free-vector/realistic-receipt-template_23-2147938550.jpg?w=2000",
+		Member:  MemberA,
+		Course:  CourseA,
+	}
+	db.Model(&CourseRegistration{}).Create(&CourseRegistrationA)
 
-  PaymentStatusA := PaymentStatus{
-    Status:  "จ่ายเงินแล้ว",
-  }
-  db.Model(&PaymentStatus{}).Create(&PaymentStatusA)
+	PaymentStatusA := PaymentStatus{
+		Status: "จ่ายเงินแล้ว",
+	}
+	db.Model(&PaymentStatus{}).Create(&PaymentStatusA)
 
-  PaymentCheckA := PaymentCheck{
-    Comment:            "จ่ายเงินถูกต้อง",
-    Employee:           EmployeeA,
-    PaymentStatus:      PaymentStatusA,
-    CourseRegistration: CourseRegistrationA,
-  }
-  db.Model(&PaymentCheck{}).Create(&PaymentCheckA)
+	PaymentCheckA := PaymentCheck{
+		Comment:            "จ่ายเงินถูกต้อง",
+		Employee:           EmployeeA,
+		PaymentStatus:      PaymentStatusA,
+		CourseRegistration: CourseRegistrationA,
+	}
+	db.Model(&PaymentCheck{}).Create(&PaymentCheckA)
 
-  QuestionA := Question{
-    Title:    "จ่ายเงินอย่างไร",
-    Detail:   "ผมสงสัยมากช่วยผมหน่อย",
-    Member:   MemberA,
-  }
-  db.Model(&Question{}).Create(&QuestionA)
+	QuestionA := Question{
+		Title:  "จ่ายเงินอย่างไร",
+		Detail: "ผมสงสัยมากช่วยผมหน่อย",
+		Member: MemberA,
+	}
+	db.Model(&Question{}).Create(&QuestionA)
 
-  ReplyA := Reply{
-    Title:    "จ่ายเงินแบบนี้",
-    Detail:   "ต้องไปจ่ายในระบบจ่ายเงิน",
-    Question: QuestionA,
-    Employee: EmployeeA,
-  }
-  db.Model(&Reply{}).Create(&ReplyA)
+	ReplyA := Reply{
+		Title:    "จ่ายเงินแบบนี้",
+		Detail:   "ต้องไปจ่ายในระบบจ่ายเงิน",
+		Question: QuestionA,
+		Employee: EmployeeA,
+	}
+	db.Model(&Reply{}).Create(&ReplyA)
 
-  NewsA := News{
-    Title:    "ฟังก์ชันใหม่",
-    Detail:   "ตอนนี้ทางเว็บเรามีฟังก์ชันใหม่แล้วนะครับ",
-    Admin:    AdminA,
-  }
-  db.Model(&News{}).Create(&NewsA)
+	NewsA := News{
+		Title:  "ฟังก์ชันใหม่",
+		Detail: "ตอนนี้ทางเว็บเรามีฟังก์ชันใหม่แล้วนะครับ",
+		Admin:  AdminA,
+	}
+	db.Model(&News{}).Create(&NewsA)
 
 }
--- frontend/src/page/register/Register.tsx
+++ frontend/src/page/register/Register.tsx
@@ -1,32 +1,60 @@
 import React from "react";
 import "./Register.css";
 import MemberReg1 from "../../assets/MemberLogin1.png"
-import Topbar from "../../component/TopBar/Topbar";
+import TopbarReg from "../../component/TopBar/TopbarReg";
+import { MemberInterface } from "../../interfaces/IMember";
 
 function Register() {
+
   return (
     <div>
-      <Topbar />
+      <TopbarReg />
       <div className="wrapper-reg">
+      <div className="wrapper-reg2">
         <div className="photo-reg">
           <img src={MemberReg1} />
         </div>
         <div className="from-reg">
           <h3 className="header-reg">ลงทะเบียน</h3>
           <div className="input-namereg">
-            <input placeholder="ชื่อ"></input>
-            <input placeholder="สกุล"></input>
+            <input className="input-namereg-text" placeholder="ชื่อ"></input>
+            <input className="input-namereg-text" placeholder="สกุล"></input>
           </div>
           <div className="input-allreg">
-            <input placeholder="อีเมล"></input>
-            <input placeholder="รหัสผ่าน"></input>
-            <input placeholder="ยืนยันรหัสผ่าน"></input>
-            <input placeholder="เบอร์โทรศัพท์"></input>
-            <input placeholder="ที่อยู๋"></input>
+            <input className="input-allin" placeholder="อีเมล"></input>
+            <input className="input-allin" placeholder="รหัสผ่าน"></input>
+            <input className="input-allin" placeholder="ยืนยันรหัสผ่าน"></input>
+            <input className="input-allin" placeholder="เบอร์โทรศัพท์"></input>
+            <input className="input-allin" placeholder="ที่อยู่"></input>
+          </div>
+          <div>
+          <div className="layout-sexdate">
+            <div className="form-sexreg">
+              เพศ
+            </div>
+            <div className="form-sexreg">
+              วันเกิด
+            </div>
           </div>
           <div className="Date-sex-reg">
-            <div className="input-sexreg"></div>
-            <div className="input-datereg"></div>
+            <div className="input-sexreg">
+              <label className="container-reg">หญิง
+                <input type="radio" name="radio" />
+                <span className="checkmark-reg"></span>
+              </label>
+              <label className="container-reg">ชาย
+                <input type="radio" name="radio"/>
+                <span className="checkmark-reg"></span>
+              </label>
+              <label className="container-reg">ไม่ระบุ
+                <input type="radio" name="radio"/>
+                <span className="checkmark-reg"></span>
+              </label>
+            </div>
+            <div className="input-datereg">
+              <input type="date" name="date-reg"/>
+            </div>
+          </div>
           </div>
           <div className="form-bntreg">
             <button className="input-bntreg">
@@ -34,6 +62,7 @@ function Register() {
             </button>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
